4d9e7903aaab50b0292f29084803d35ffe4ddf32
MankX58 <manuelguvi500@gmail.com>
2024-02-19 08:49:27 -0500

    title and icon change

--- src/components/MainP.jsx
+++ src/components/MainP.jsx
@@ -9,7 +9,7 @@ import Button from "./Button";
 export default function MainP() {
   return (
     <main>
-      <div className=" h-[40rem] bg-gradient-to-b from-[#f7ef99] via-[#f7ef99] to-[#F9DEC9] ">
+      <div className=" h-[40rem] bg-gradient-to-b from-[#f7ef99] via-[#f7ef99] to-[#F9DEC9] bg-">
         <img
           src="./public/img/joy.png"
           alt=""
@@ -20,19 +20,24 @@ export default function MainP() {
           MANKX´S SHOP
         </h1>
         <p class=" mb-[.5rem] w-[50rem] p-[1rem] relative top-[2rem]">
-          Mi experiencia en desarrollo web me ha brindado la oportunidad de
-          colaborar estrechamente con diversos profesionales que desempeñan
-          roles similares. A lo largo de mi trayectoria, he tenido la fortuna de
-          interactuar con diseñadores, desarrolladores, especialistas en UX/UI,
-          y otros expertos del campo. Estas interacciones han enriquecido mi
-          perspectiva y me han permitido comprender la importancia del trabajo
-          en equipo y la comunicación efectiva en el desarrollo de proyectos
-          web. A través de estas experiencias, he aprendido a valorar la
-          diversidad de enfoques y habilidades, lo que me ha ayudado a crecer
-          tanto a nivel profesional como personal. Además, el intercambio de
-          ideas y conocimientos con colegas del desarrollo web me ha permitido
-          adquirir una visión más amplia de las tendencias y prácticas actuales
-          en la industria.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque nihil
+          praesentium quaerat optio illo corporis ipsam! Ratione, magni!
+          Dignissimos nihil doloremque alias saepe perspiciatis porro debitis
+          numquam, necessitatibus voluptatum nulla. Natus, vel. Quidem magnam
+          consequatur sit dolorum sequi repellendus totam magni perferendis
+          fugit, nobis cupiditate quo voluptate qui esse eius blanditiis tempore
+          hic! Error vitae laboriosam, quaerat adipisci accusamus minus!
+          Voluptatum provident delectus harum quis cumque minima itaque
+          excepturi quasi officiis porro, ab necessitatibus eaque tempore
+          deserunt iste non aut laudantium odio fugiat temporibus at nam rem eos
+          placeat. Inventore? Blanditiis veritatis omnis incidunt assumenda
+          velit sint molestias aperiam. Amet nostrum fugit nobis. Dolorem
+          facere, esse ratione temporibus, neque nulla est officiis, ab non modi
+          quasi nemo accusantium eum aliquid. Eligendi, asperiores? Hic,
+          deserunt quisquam voluptatum ipsum suscipit quia dolorum consectetur
+          iste fuga nulla deleniti quibusdam placeat voluptatibus dolorem eius
+          molestiae eum totam, quasi laborum voluptates! Optio ut similique
+          officia!
         </p>
         <br />
         <div className="relative left-[1rem]">
