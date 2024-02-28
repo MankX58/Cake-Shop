49dd160641d091d9afff629324bb6af0de081ca6
MankX58 <manuelguvi500@gmail.com>
2024-02-28 13:27:17 -0500

    yea

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ function Button() {
       }}
       className="bg-[#938ba1] w-[7rem] h-[3rem] rounded-[10px] hover:outline hover:outline-[2px] hover:scale-[1.10] transition m-[.4rem]"
     >
-      Hola mundo
+      Conoce más
     </button>
   );
 }
--- src/components/MainP.jsx
+++ src/components/MainP.jsx
@@ -10,42 +10,47 @@ export default function MainP() {
   return (
     <main>
       <div className=" h-[40rem]" id="header-div">
-        <div className="flex flex-col justify-center items-center mt-[1rem]">
+        <div className="flex flex-row gap-[6rem] max-sm:gap-[2rem] max-md:gap-[3rem] justify-center items-center pt-[1rem] bg-[#f5db87] h-[34rem] mb-[4.5rem]">
           <img
             src="./public/img/pastel_main.webp"
             alt=""
-            class="w-[400px] h-[350px] ml-[2rem] mb-[1rem] right-[1rem] relative max-xl:w-[300px] max-xl:h-[250px]"
+            class="w-[400px] h-[350px] ml-[2rem] mb-[1rem] right-[1rem] relative max-xl:w-[300px] max-xl:h-[250px] max-lg:w-[280px] max-lg-[150px] max-md:w-[150px] max-md:h-[120px] max-sm:ml-[5rem]"
             id="pastel-main"
           />
-          <h1 className="font-bebas_Neue text-[100px] max-xl:text-[80px]">
-            ¡BIENVENIDO!
+          <h1 className="font-bebas_Neue text-[90px] max-xl:text-[80px] max-md:text-[60px] max-sm:text-[40px] max-sm:mr-[3rem]">
+            ¡BIENVENIDO A{" "}
+            <h1 className="font-pacifico text-[60px] font-bold max-md:text-[40px] max-sm:text-[25px] max max-sm:mr-[3rem]">
+              MANKX´S SHOP!
+            </h1>
           </h1>
         </div>
-        <h1 className="relative top-[2.5rem] text-xl font-bold left-[1rem]">
-          ManelGuvi300 Shop
-        </h1>
-        <p class=" mb-[.5rem] w-[50rem] p-[1rem] relative top-[2rem]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque nihil
-          praesentium quaerat optio illo corporis ipsam! Ratione, magni!
-          Dignissimos nihil doloremque alias saepe perspiciatis porro debitis
-          numquam, necessitatibus voluptatum nulla. Natus, vel. Quidem magnam
-          consequatur sit dolorum sequi repellendus totam magni perferendis
-          fugit, nobis cupiditate quo voluptate qui esse eius blanditiis tempore
-          hic! Error vitae laboriosam, quaerat adipisci accusamus minus!
-          Voluptatum provident delectus harum quis cumque minima itaque
-          excepturi quasi officiis porro, ab necessitatibus eaque tempore
-          deserunt iste non aut laudantium odio fugiat temporibus at nam rem eos
-          placeat. Inventore? Blanditiis veritatis omnis incidunt assumenda
-          velit sint molestias aperiam. Amet nostrum fugit nobis. Dolorem
-          facere, esse ratione temporibus, neque nulla est officiis, ab non modi
-          quasi nemo accusantium eum aliquid. Eligendi, asperiores? Hic,
-          deserunt quisquam voluptatum ipsum suscipit quia dolorum consectetur
-          iste fuga nulla deleniti quibusdam placeat voluptatibus dolorem eius
-          molestiae eum totam, quasi laborum voluptates! Optio ut similique
-          officia!
-        </p>
+        <section className="text-center items-center justify-center">
+          <h1 className="font-bebas_Neue font-bold text-[40px]">
+            Nuestros Productos
+          </h1>
+
+          {/* ///////////////////// */}
+          {/* TARJETAS DE PRODUCTOS */}
+          <section className="flex shrink-0 flex-wrap transition justify-center">
+            <div className="card">
+              <img src="public\img\pastel_arequipe.jpg" className="" alt="" />
+              <h1>Pastel de arquipe</h1>
+              <Button></Button>
+            </div>
+
+            <div className="card">
+              <Button></Button>
+            </div>
+
+            <div className="card">
+              <Button></Button>
+            </div>
+          </section>
+
+          {/* ///////////////////// */}
+        </section>
         <br />
-        <div className="relative left-[1rem]">
+        <div className="relative left-[1rem] text-center justify-center items-center">
           <Button />
           <Button />
           <Button />
