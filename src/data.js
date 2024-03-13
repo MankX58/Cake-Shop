6a24637b7fad7efbb9f41262a389ab6909fc9f49
Manuel <manuelguvi500@gmail.com>
2024-03-12 22:38:41 -0500

    alaro src

--- src/data.js
+++ src/data.js
@@ -44,6 +44,6 @@ export const products = [
       id: 8,
       title: "Alaro",
       price: 50,
-      src: "#"
+      src: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.helencummins.es%2Falaro-actividades%2F&psig=AOvVaw2bU8y9ne7ehkDS_yeDtdIL&ust=1710110007829000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIDR1aye6IQDFQAAAAAdAAAAABAE"
     }
   ];
